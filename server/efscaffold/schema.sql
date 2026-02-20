38d29184c740e6553ebbe2fcb92c85ecac4d4172
LeoMurjinschi <leomurjinschi@gmail.com>
2026-02-20 17:29:41 +0200

    version control

---
+++ server/efscaffold/schema.sql
@@ -0,0 +1,9 @@
+DROP SCHEMA IF EXISTS ToDoSystem CASCADE;
+CREATE SCHEMA IF NOT EXISTS ToDoSystem;
+
+CREATE TABLE ToDoSystem.todo(
+    id text not null primary key,
+    title text not null,
+    description text not null,
+    priority numeric not null
+)
