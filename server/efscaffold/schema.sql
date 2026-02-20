0dce0364120480ef0d76522c1df5d85b62a978f4
LeoMurjinschi <leomurjinschi@gmail.com>
2026-02-20 18:42:47 +0200

    managed NuGet Packages and updated todo table structure

--- server/efscaffold/schema.sql
+++ server/efscaffold/schema.sql
@@ -5,5 +5,6 @@ CREATE TABLE ToDoSystem.todo(
     id text not null primary key,
     title text not null,
     description text not null,
-    priority numeric not null
+    priority numeric not null,
+    isdone boolean not null
 )
